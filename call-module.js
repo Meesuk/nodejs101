f0bbb239952e12889ba7205c84397d887e1f2855
meesuk <meesuk_programmer@hotmail.com>
2025-07-11 11:40:24 +0700

    module - destructuring

--- call-module.js
+++ call-module.js
@@ -1,6 +1,7 @@
-const myModule = require('./modules/my-module')
-const getCurrentTime = require('./modules/my-module').getCurrentTime
+/* const myModule = require('./modules/my-module')
+const getCurrentTime = require('./modules/my-module').getCurrentTime */
 
-console.log(myModule.getCurrentTime())
+const {getCurrentTime, formatMoney} = require('./modules/my-module')
+
 console.log(getCurrentTime())
-console.log(myModule.formatMoney(5000000))
+console.log(formatMoney(5000000))
