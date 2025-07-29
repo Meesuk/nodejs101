89d135a4d8364d74b7ec9816b1ff05a237b74058
meesuk <meesuk_programmer@hotmail.com>
2025-07-29 18:08:58 +0700

    apis

--- http.js
+++ http.js
@@ -1,12 +1,13 @@
 const http = require('http')
 const path = require('path')
 const fs = require('fs')
+const moment = require('moment')
 
 function getPage(page) {
     return fs.readFileSync(path.join(__dirname, page))
 }
 
-http.createServer((req, res) => {
+function handlePage(req, res) {
     const fileType = path.extname(req.url) || '.html'
     
     if (fileType === '.html') {
@@ -25,6 +26,35 @@ http.createServer((req, res) => {
     } else {
         res.writeHead(404)
         res.end()
+    }
+}
+
+function handleAPIs(req, res) {
+    let data
+    if (req.url === '/apis/users') {
+        data = [{name: 'John'}, {name: 'Sara'}]
+    } else if (req.url === '/apis/posts') {
+        data = [
+            {title: 'A', publishedDate: moment().startOf('day').fromNow()},
+            {title: 'B', publishedDate: moment().set('month', 1).startOf('day').fromNow()}
+        ]
+    }
+
+    if (data) {
+        res.writeHead(200, {'Content-Type': 'application/json'})
+        res.write(JSON.stringify(data))
+    } else {
+        res.writeHead(404)
+    }
+
+    res.end()
+}
+
+http.createServer((req, res) => {
+    if (req.url.startsWith('/apis/')) {
+        handleAPIs(req, res)
+    } else {
+        handlePage(req, res)
     }
 }).listen(3000, () => {
     console.log(`Server is running on http://localhost:3000`)
